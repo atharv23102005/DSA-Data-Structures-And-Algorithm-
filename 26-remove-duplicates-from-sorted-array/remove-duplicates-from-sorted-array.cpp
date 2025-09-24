88040f370078cf41f080bf52ea4f7e9efeb6f58d
Atharva Srivastava <atharvasrivastava2023@gmail.com>
2025-09-24 13:25:57 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (96.56%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        auto it=unique(nums.begin(),nums.end());
+        nums.erase(it , nums.end());
+     return nums.size();
+    }
+};
