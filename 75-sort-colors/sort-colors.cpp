b61c352f280d71ec597b63a7f5dd02b1ac7127ec
Atharva Srivastava <atharvasrivastava2023@gmail.com>
2025-10-02 14:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (79.05%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 = 0, count1=0,count2=0;
-        int n=nums.size();
-        for(int i=0;i <n;i++){
-            if(nums[i] == 0){
-            count0++;
-            }
-            else if(nums[i]== 1){
-                count1++;
-            }
-            else{
-                count2++;
-            }
-            }
+      //  int count0 = 0, count1=0,count2=0;
+     //   int n=nums.size();
+     //   for(int i=0;i <n;i++){
+      //      if(nums[i] == 0){
+      //      count0++;
+      //      }
+       //     else if(nums[i]== 1){
+      //          count1++;
+         //   }
+           // else{
+                ///count2++;
+            ////}
+          //  }
 
             ///////
             
-            for( int i=0; i < count0; i++){
-            nums[i]=0;
+    //        for( int i=0; i < count0; i++){
+       //     nums[i]=0;
+         //   }
+      //      for(int i=count0 ;i< count0+count1 ;i++){
+      //          nums[i]=1;
+    //        }
+      //      for(int i=count0+count1 ; i<n; i++){
+        //        nums[i]=2;
+       //     }
+int low=0,mid=0,high=nums.size()-1;
+int n=nums.size();
+        
+        while(mid <=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
-            for(int i=count0 ;i< count0+count1 ;i++){
-                nums[i]=1;
+            else if(nums[mid]==1){
+                mid++;
             }
-            for(int i=count0+count1 ; i<n; i++){
-                nums[i]=2;
-            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
 
-        
-        
+            }
+        }
+
     }
 };
