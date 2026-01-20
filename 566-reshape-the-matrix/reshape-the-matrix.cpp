56b80aaf5d4386c79e1458206edc96aba7da780f
Atharva Srivastava <atharvasrivastava2023@gmail.com>
2026-01-20 17:19:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (55.93%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+
+        // Check if reshape is possible
+        if (rows * cols != r * c)
+            return mat;
+
+        vector<vector<int>> result(r, vector<int>(c));
+        int idx = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[idx / c][idx % c] = mat[i][j];
+                idx++;
+            }
+        }
+        return result;
+    }
+};
